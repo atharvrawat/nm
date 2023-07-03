7f7ad5ec3ef01b3d34287e4f6ffa1a3cd7f2dcca
atharvrawat <109080313+atharvrawat@users.noreply.github.com>
2023-07-03 17:47:35 +0530

    Add files via upload

---
+++ main_1.js
@@ -0,0 +1,49 @@
+status = "";
+img = "";
+objects = []
+function preload() {
+    img = loadImage("pic_1.jpg")
+}
+
+function setup() {
+    canvas = createCanvas(640, 420)
+    canvas.center()
+    object_detector = ml5.objectDetector("cocossd", modelloaded);
+    document.getElementById("status").innerHTML = "status:detecting objects";
+
+}
+
+function draw() {
+    image(img, 0, 0, 640, 420)
+    if (status != "") {
+        for (i = 0; i < objects.length; i++) {
+            document.getElementById("status").innerHTML = "status:Object Detected";
+            fill("red")
+            stroke("red")
+            percent = floor(objects[i].confidence * 100)
+            text(objects[i].label+" "+percent+"%",objects[i].x,objects[i].y);
+            noFill();
+            rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height)
+        }
+    }
+
+
+}
+
+function modelloaded() {
+    console.log("model is loaded")
+    status = true;
+    object_detector.detect(img, gotresults);
+
+}
+
+function gotresults(error, results) {
+    if (error) {
+        console.log(error)
+
+    }
+    else {
+        console.log(results);
+        objects = results
+    }
+}
